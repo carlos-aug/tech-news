62f55cf59e9547b0db1aeb010607322fea5ca914
Carlos Augusto <caarlos.san@gmail.com>
2023-04-18 15:30:55 -0300

    feat: add get_tech_news with help of Gustavo Reis

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,9 +2,10 @@ import requests
 import time
 from parsel import Selector
 
+from tech_news.database import create_news
+
+
 # Requisito 1
-
-
 def fetch(url):
     headers = {"user-agent": "Fake user-agent"}
     time.sleep(1)
@@ -36,7 +37,7 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_news(html_content):
 
-    selector = Selector(text=html_content, type="html")
+    selector = Selector(text=html_content)
     url_link = selector.css('link[rel="canonical"]::attr(href)').get()
     title = selector.css(".entry-title::text").get().strip()
     timestamp = selector.css(".meta-date::text").get()
@@ -48,8 +49,8 @@ def scrape_news(html_content):
         "div.entry-content:first-of-type > p:nth-of-type(1) ::text"
     ).getall()
     summary = "".join(summary).strip()
-
     category = selector.css(".label::text").get().strip()
+
     new_dict = {
         "url": url_link,
         "title": title,
@@ -65,11 +66,24 @@ def scrape_news(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = "https://blog.betrybe.com"
+    data_pages = fetch(url)
+    data_links = []
+
+    while len(data_links) < amount:
+        data_links.extend(scrape_updates(data_pages))
+        data_pages = fetch(scrape_next_page_link(data_pages))
+
+    result_scrape = []
+    for link in data_links[:amount]:
+        fetch_links = fetch(link)
+        result_scrape.append(scrape_news(fetch_links))
+
+    create_news(result_scrape)
+    return result_scrape
 
 
-if __name__ == "__main__":
-    url = (
-        "https://blog.betrybe.com/desenvolvimento-web/estruturas-de-repeticao/"
-    )
-    print(scrape_news(fetch(url)))
+# if __name__ == "__main__":
+#     url = "https://blog.betrybe.com"
+#     page_content = fetch(url)
+#     print(get_tech_news(5))
